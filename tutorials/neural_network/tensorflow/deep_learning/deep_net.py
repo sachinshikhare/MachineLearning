459ad505798ac06be063432ea506e8d2ee31c2b5
sachinshikhare <shikhare.sachin@gmail.com>
2018-05-03 22:19:27 +0530

    updated

--- tutorials/neural_network/tensorflow/deep_learning/deep_net.py
+++ tutorials/neural_network/tensorflow/deep_learning/deep_net.py
@@ -1,26 +1,36 @@
-"""
-Feed forward neural network:
-input data > weight > hidden layer 1 (activation function 1) > weight > hidden layer 2 (activation function)
-> weights > output layer
+import tensorflow as tf
 
-compare output to intended output -> cost function 
+'''
+input > weight > hidden layer 1 (activation function) > HL2 (AF) > weights > output layer
 
-optimization function (optimizer) -> minimize cost (AdamOptimizer, StochaticGradientDesccent)
+compare output to intended output > cost function
+
+optimizer > minimiza cost
 
 backpropagation
 
-feedforward + backpropagation = epoch
-"""
+feed forward + backpropagation  = epoch
+'''
 
-import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
 n_nodes_hl3 = 500
 
+"""# Gives 0.955 accuracy
+n_nodes_hl1 = 700
+n_nodes_hl2 = 700
+n_nodes_hl3 = 700
+"""
+"""
+# Gives 0.35 accuracy
+n_nodes_hl1 = 20
+n_nodes_hl2 = 30
+n_nodes_hl3 = 40
+"""
 n_classes = 10
 batch_size = 100
 
@@ -28,26 +38,25 @@ x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
 
 def neural_network_model(data):
-    hidden_1_layer = {"weights": tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      "biases": tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    # (input_data * weighhts) + bias
 
-    hidden_2_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      "biases": tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])), 'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])), 'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])), 'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
-    hidden_3_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      "biases": tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])), 'biases': tf.Variable(tf.random_normal([n_classes]))}
 
-    output_layer = {"weights": tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                      "biases": tf.Variable(tf.random_normal([n_classes]))}
-    	
-    # (input_data * weights) + biases
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+    # removing bias gives almost same accuracy
+    # l1 = tf.matmul(data, hidden_1_layer['weights'])
     l1 = tf.nn.relu(l1)
 
     l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+    # l2 = tf.matmul(l1, hidden_2_layer['weights'])
     l2 = tf.nn.relu(l2)
 
     l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+    # l3 = tf.matmul(l2, hidden_3_layer['weights'])
     l3 = tf.nn.relu(l3)
 
     output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
@@ -59,19 +68,49 @@ def train_neural_network(x):
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    hm_epoch = 10
+    hm_epochs = 10
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for epoch in range(hm_epoch):
+
+        for epoch in range(hm_epochs):
             epoch_loss = 0
             for _ in range(int(mnist.train.num_examples / batch_size)):
                 epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                temp, c = sess.run([optimizer, cost], feed_dict = {x:epoch_x, y:epoch_y})
+                _, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
+
                 epoch_loss += c
-            print("Epoch", epoch, 'completed out of', hm_epoch, 'Lost', epoch_loss)
+            print('Epoch ', epoch, ' completed oout of ', hm_epochs, 'loss: ', epoch_loss)
+
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        print('Accuracy', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
 
 train_neural_network(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
